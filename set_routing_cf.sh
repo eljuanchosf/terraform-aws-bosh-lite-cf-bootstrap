bd86ae6fe55409a352a260f15da14c1fa0a0b10a
Juan Pablo <juanpgenovese@gmail.com>
2016-05-17 15:35:35 -0300

    Add ports

--- set_routing_cf.sh
+++ set_routing_cf.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j DNAT --to-destination 10.244.0.34
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 10.244.0.34
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 2222 -j DNAT --to-destination 10.244.0.34
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 4443 -j DNAT --to-destination 10.244.0.34
